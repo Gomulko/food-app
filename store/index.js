6d9f6af1fe92632510932c7c7d9d52cb34e56945
Gomulko <rafa.gomu1994@gmail.com>
2022-07-02 11:26:19 +0200

    fix store

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-export const state = () => {
+export const state = () => ({
     fooddata: [{
         name: "Taco bout Eating Tacos",
         deliveryTime: 25,
@@ -14,8 +14,8 @@ export const state = () => {
             options: ["Al Pastor", "Carnitas", "Lengua", "Carne Asada", "Pollo"],
             addOns: ["none", "sour cream", "avocado", "pico de gallo", "queso"],
             description: "Soft tortilla, your choice of meat, onions, cilantro & salsa"
-        },
-        {
+          },
+          {
             item: "Quesadilla",
             img: "taco-quesadilla.png",
             price: 7.0,
@@ -23,8 +23,8 @@ export const state = () => {
             options: ["Plain", "Carne Asada", "Pollo"],
             addOns: ["none", "sour cream", "guacamole", "pico de gallo"],
             description: "Flour tortilla, melted cheese & optional meat choice"
-        },
-        {
+          },
+          {
             item: "Burrito",
             img: "taco-burrito.jpg",
             price: 6.59,
@@ -32,10 +32,10 @@ export const state = () => {
             options: ["Al Pastor", "Carnitas", "Lengua", "Carne Asada", "Pollo"],
             addOns: ["none", "sour cream", "avocado", "pico de gallo", "queso"],
             description: "Any meat, rice, beans, onions, cilantro & salsa"
-        }
+          }
         ]
-    },
-    {
+      },
+      {
         name: "A Real Pizza Work",
         deliveryTime: 40,
         meal: ["lunch", "dinner"],
@@ -46,56 +46,56 @@ export const state = () => {
             item: "Build Your Own",
             img: "pizza-plain.jpg",
             addOns: [
-                "None",
-                "Sausage",
-                "Pepperoni",
-                "Tomatoes",
-                "Mushrooms",
-                "Onion",
-                "Green Pepper",
-                "Extra Cheese"
+              "None",
+              "Sausage",
+              "Pepperoni",
+              "Tomatoes",
+              "Mushrooms",
+              "Onion",
+              "Green Pepper",
+              "Extra Cheese"
             ],
             price: 17.99,
             id: "7644eba9-d6a8-466f-bd49-781bc005cfe5",
             description: "Design your own pizza"
-        },
-        {
+          },
+          {
             item: "MeatLovers",
             img: "pizza-meat.jpg",
             price: 17.99,
             addOns: [
-                "None",
-                "Sausage",
-                "Pepperoni",
-                "Tomatoes",
-                "Mushrooms",
-                "Onion",
-                "Green Pepper",
-                "Extra Cheese"
+              "None",
+              "Sausage",
+              "Pepperoni",
+              "Tomatoes",
+              "Mushrooms",
+              "Onion",
+              "Green Pepper",
+              "Extra Cheese"
             ],
             id: "70ceec44-3ccb-4f92-b149-0ebe2c10704b",
             description: "A Carnivore’s delight. Pepperoni, Sausage, Salami & Canadian Bacon."
-        },
-        {
+          },
+          {
             item: "Veggie",
             img: "pizza-veggie.jpg",
             price: 17.99,
             addOns: [
-                "None",
-                "Sausage",
-                "Pepperoni",
-                "Tomatoes",
-                "Mushrooms",
-                "Onion",
-                "Green Pepper",
-                "Extra Cheese"
+              "None",
+              "Sausage",
+              "Pepperoni",
+              "Tomatoes",
+              "Mushrooms",
+              "Onion",
+              "Green Pepper",
+              "Extra Cheese"
             ],
             id: "b27f4d8e-7235-42f9-b0a7-98e76a1afaf0",
             description: "Tomatoes, Olives, Garlic, Onions, Mushrooms & Bells."
-        }
+          }
         ]
-    },
-    {
+      },
+      {
         name: "All that and Dim Sum",
         deliveryTime: 35,
         meal: ["breakfast", "lunch", "dinner"],
@@ -107,41 +107,59 @@ export const state = () => {
             img: "dimsum-shrimp.jpg",
             price: 6.49,
             addOns: [
-                "None",
-                "Steamed Sesame Ball",
-                "Mango Pudding",
-                "Sweet Tofu"
+              "None",
+              "Steamed Sesame Ball",
+              "Mango Pudding",
+              "Sweet Tofu"
             ],
             id: "22672b93-2c65-4fd9-b151-683f7eb7df4a",
             description: "Whole shrimp in a translucent wrapper."
-        },
-        {
+          },
+          {
             item: "Soup Dumplings",
             img: "dimsum-soup.jpg",
             price: 9.99,
             addOns: [
-                "None",
-                "Steamed Sesame Ball",
-                "Mango Pudding",
-                "Sweet Tofu"
+              "None",
+              "Steamed Sesame Ball",
+              "Mango Pudding",
+              "Sweet Tofu"
             ],
             id: "3a6da02c-2354-4eb8-af07-f9f2d26fa7c2",
             description: "Dumplings filled with pork, crab meat and broth."
-        },
-        {
+          },
+          {
             item: "Stuffed Eggplant",
             img: "dimsum-eggplant.jpg",
             price: 5.59,
             addOns: [
-                "None",
-                "Steamed Sesame Ball",
-                "Mango Pudding",
-                "Sweet Tofu"
+              "None",
+              "Steamed Sesame Ball",
+              "Mango Pudding",
+              "Sweet Tofu"
             ],
             id: "cadd2903-b9cb-4053-9720-c096cc95e8a5",
             description: "Chinese eggplant filled with ground shrimp in sauce"
-        }
+          }
         ]
-    }
+      }
     ]
-}
+  })
+  
+  // export const getters = {
+  //   getterValue: state => {
+  //     return state.value
+  //   }
+  // }
+  
+  // export const mutations = {
+  //   updateValue: (state, payload) => {
+  //     state.value = payload
+  //   }
+  // }
+  
+  // export const actions = {
+  //   updateActionValue({ commit }) {
+  //     commit('updateValue', payload)
+  //   }
+  // }
